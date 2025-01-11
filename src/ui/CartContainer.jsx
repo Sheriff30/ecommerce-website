9220848fc960f1117f32fb2082cd109b018ea1e5
Sherif Fouad <sherifdevv@gmail.com>
2025-01-11 06:58:23 +0200

    added number of cart items above cart icon

--- src/ui/CartContainer.jsx
+++ src/ui/CartContainer.jsx
@@ -1,9 +1,12 @@
 import { CiShoppingCart } from "react-icons/ci";
 import Cart from "./Cart";
 import { useState, useRef, useEffect } from "react";
+import useCartStore from "../store/cartStore";
 
 function CartContainer() {
   const [showCart, setShowCart] = useState(false);
+  const { cartItems } = useCartStore();
+
   const cartRef = useRef(null);
   const cartIconRef = useRef(null);
 
@@ -32,8 +35,13 @@ function CartContainer() {
 
   return (
     <div>
-      <div ref={cartIconRef} onClick={handleCartDisplay}>
+      <div ref={cartIconRef} onClick={handleCartDisplay} className="relative">
         <CiShoppingCart size={36} className="cursor-pointer" />
+        {cartItems.length ? (
+          <span className="text-white absolute -top-1 -right-1 bg-black border-2  rounded-[50%] h-6 w-6 text-center flex justify-center items-center ">
+            <div>{cartItems.length}</div>
+          </span>
+        ) : null}
       </div>
 
       {showCart && (
